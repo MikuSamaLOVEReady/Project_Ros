8ef8bee41ed2fa8e6477b0610f65a36536c147e3
liruirui <2954701669@qq.com>
2023-08-02 18:35:25 -0700

    rui

--- src/ros_merge_test/src/align.cpp
+++ src/ros_merge_test/src/align.cpp
@@ -3,10 +3,56 @@
 namespace uwb_slam{
     void Align::Run()
     {
-        ros::Rate loop_rate(10);
+        tmp = ros::Time::now();
+        ros::Time tmp1 = ros::Time::now();
+        // ros::Rate loop_rate(10);
         wheel_odom_sub_= nh_.subscribe("wheel_odom",10,&Align::wheel_odomCB,this);
         imu_sub_= nh_.subscribe("raw_imu",10,&Align::imuCB,this);
-    }
+        std::ofstream outfile("data.txt",std::ofstream::out);
+        if(outfile.is_open()){
+        int count = 0;
+        outfile <<"imu_odom_: "<< "imu_timestamp"<<"imu_linear_acc_x_y_z"<<"imu_angular_x_y_z"<<
+                "odom_vxy"<<"odom_angle_v_"<<"\n";
+        while(1){
+            
+            if(tmp!=imu_odom_.imu_data_.imu_t_){
+                count++;
+                outfile <<"imu_odom_: "<< "imu_timestamp  "<<"imu_linear_acc_x_y_z  "<<"imu_angular_x_y_z  "<<
+                "odom_vxy  "<<"odom_angle_v_  "<<"\n";
+
+                if(tmp1!=uwb_->uwb_data_.uwb_t_){
+                     outfile <<"imu_odom_: "<< imu_odom_.imu_data_.imu_t_ <<"*"<<imu_odom_.imu_data_.a_[0]<<"*"<<
+                     imu_odom_.imu_data_.a_[1]<<"*"<<imu_odom_.imu_data_.a_[2]<<"*"<<imu_odom_.imu_data_.w_[0]<<"*"<<imu_odom_.imu_data_.w_[1]<<"*"<<imu_odom_.imu_data_.w_[2]<<"*"<<imu_odom_.vxy_<<"*"<<
+                    imu_odom_.angle_v_<<
+                    "uwb: "<<uwb_->uwb_data_.uwb_t_<<"*"<<uwb_->uwb_data_.x_<<"*"<<uwb_->uwb_data_.y_<<"\n";
+                     tmp1 = uwb_->uwb_data_.uwb_t_;
+                 }
+               else{
+                outfile <<"imu_odom_: "<< imu_odom_.imu_data_.imu_t_ <<"*"<<imu_odom_.imu_data_.a_[0]<<"*"<<
+                    imu_odom_.imu_data_.a_[1]<<"*"<<imu_odom_.imu_data_.a_[2]<<"*"<<imu_odom_.imu_data_.w_[0]<<"*"<<imu_odom_.imu_data_.w_[1]<<"*"<<imu_odom_.imu_data_.w_[2]<<"*"<<imu_odom_.vxy_<<"*"<<
+                    imu_odom_.pose_[0]<<"*"<<imu_odom_.pose_[0]<<"*"<<imu_odom_.pose_[0]<<
+                    "uwb: "<<uwb_->uwb_data_.uwb_t_<<"\n";
+                    std::cout << "uwb_data_: " << uwb_data_.uwb_t_<< std::endl;
+                    //tmp = imu_odom_.imu_data_.imu_t_;
+               }
+                tmp = imu_odom_.imu_data_.imu_t_;
+                    
+                // tmp1 = uwb_->uwb_data_.uwb_t_;
+                
+                
+            }
+            if(count>300)
+                break;
+        }
+
+        outfile.close();
+        std::cout<< "Data written to file." << std::endl;
+        }
+        else{
+            std::cout<<"file can not open"<<std::endl;
+        }
+        }
+    
     
     void Align::wheel_odomCB(const nav_msgs::Odometry& wheel_odom)
     {
@@ -19,6 +65,7 @@ namespace uwb_slam{
         imu_odom_.quat_[1] = wheel_odom.pose.pose.orientation.y;
         imu_odom_.quat_[2] = wheel_odom.pose.pose.orientation.z;
         imu_odom_.quat_[3] = wheel_odom.pose.pose.orientation.w;
+
     
         return;
     }
--- src/ros_merge_test/src/uwb.cpp
+++ src/ros_merge_test/src/uwb.cpp
@@ -43,17 +43,17 @@ namespace uwb_slam{
         serial.set_option(boost::asio::serial_port_base::parity(boost::asio::serial_port_base::parity::none)); // 设置校验位
         serial.set_option(boost::asio::serial_port_base::stop_bits(boost::asio::serial_port_base::stop_bits::one)); // 设置停止位
 
-        uint8_t tmpdata[13];
+        uint8_t tmpdata[12];
         // std::cerr << "befor read" << std::endl;
-        size_t bytesRead = boost::asio::read(serial, boost::asio::buffer(tmpdata, 13)); // 读取串口数据
+        size_t bytesRead = boost::asio::read(serial, boost::asio::buffer(tmpdata, 12)); // 读取串口数据
         // std::cerr << "after read" << std::endl;
 
         // for (int i=0;i< bytesRead;i++)
         // {
         //     std::cout << "Received data: " << std::hex<<static_cast<int>(tmpdata[i]) ;
         // }
-        memcpy(&this->distance, &tmpdata[4], sizeof(distance));
-        memcpy(&this->theta, &tmpdata[8], sizeof(theta));
+        memcpy(&this->distance, &tmpdata[3], sizeof(distance));
+        memcpy(&this->theta, &tmpdata[7], sizeof(theta));
         /*this->x = cosf(theta/180*PI)*distance+1000;
         this->y = sinf(theta/180*PI)*distance+1000;
         this->theta = theta;*/
@@ -63,13 +63,13 @@ namespace uwb_slam{
         
         // uwb_data_queue_.push(uwb_data_);
       
-        //std::cout << "PI: " << PI << std::endl;
-        cur_seq = static_cast<int>(tmpdata[3]);
+        //std::cout << "uwb_data_: " << uwb_data_.uwb_t_<< std::endl;
+        // cur_seq = static_cast<int>(tmpdata[3]);
         //std::cout << "****** cur _ sequence: " <<  cur_seq << "x: " << x << " y: " << y <<std::endl;
-        if( cur_seq  - pre_seq != 1){
-            std::cout << "****** cur _ sequence: " << cur_seq << "pre _ sequence: " << pre_seq << " ******\n";
-        }
-        pre_seq = static_cast<int>(tmpdata[3]);
+        // if( cur_seq  - pre_seq != 1){
+        //     std::cout << "****** cur _ sequence: " << cur_seq << "pre _ sequence: " << pre_seq << " ******\n";
+        // }
+        // pre_seq = static_cast<int>(tmpdata[3]);
 
         std::cout << "theta: " << theta << " distance: " << distance << std::endl;
         //std::cout std::endl;
